1c303e23e18abe2ece1d724f03ca9c42ec4bdf18
al <a.k.sugiharto@gmail.com>
2019-01-16 15:46:02 +0100

    template for trading (first version)

--- main.py
+++ main.py
@@ -68,15 +68,123 @@ def template_crypto_price():
         template_crypto_price()
 
 
+def recommendation_main(crypto):
+    #TODO process crypto
+    recommendation = {}
+    recommendation['main'] = -0.7
+    recommendation['news'] = -0.8
+    recommendation['price'] = 0.2
+    return recommendation
+
+
 def template_trade():
     for symbol in available_crypto:
         print_template("{} ({}) ".format(symbol, available_crypto[symbol]))
-    crypto = input_template("Please choose one of the above Crypto Currencies you would like to trade")
+    crypto = input_template("Please choose one of the above Crypto Currencies you'd like to trade or 'menu' to go back to main menu").upper()
+
+    #choosing menu
+    if ("MENU" in str(crypto)):
+        template_would_you_like()
+
+    #check valid crypto
+    try:
+        available_crypto[crypto]
+    except KeyError:
+        print_template("Crypto {} is not one of the available crypto to trade".format(crypto))
+        template_trade()
+
+    #analyzing for recommendation
+    recommendation = recommendation_main(crypto)
+
+    print_template("Alright, after analyzing the current news and prices of {} ({}), we recommend you...".format(available_crypto[crypto], crypto))
+    if (recommendation['main'] > 0):
+        print_template(yellow_color("to buy with certainty of {} %".format(int(recommendation['main']*100))))
+
+    else:
+        print_template(yellow_color("to not buy with certainty of {} %".format(int(recommendation['main'] * 100 * -1))))
+
+    print_template("because currently...")
+
+    if recommendation['price'] >= 0.6:
+        price_level = 'super low'
+    elif recommendation['price'] >= 0.3:
+        price_level = 'quite low'
+    elif recommendation['price'] >= 0:
+        price_level = 'a lil low'
+    elif recommendation['price'] <= -0.6:
+        price_level = 'damn high'
+    elif recommendation['price'] <= -0.3:
+        price_level = 'quite high'
+    elif recommendation['price'] < 0:
+        price_level = 'a lil high'
+
+
+    if recommendation['news'] >= 0.6:
+        news_level = 'super great'
+    elif recommendation['news'] >= 0.3:
+        news_level = 'quite good'
+    elif recommendation['news'] >= 0:
+        news_level = 'a lil oke'
+    elif recommendation['news'] <= -0.6:
+        news_level = 'super bad'
+    elif recommendation['news'] <= -0.3:
+        news_level = 'quite bad'
+    elif recommendation['news'] < 0:
+        news_level = 'a lil not oke'
+
+    if (recommendation['main']>=0):
+        #recommendation
+        recommendation_yes = ''
+        recommendation_no = 'not '
+        #choose conjuction
+        if (recommendation['price'] >= 0 and recommendation['news'] >= 0):
+            conjunction = 'and'
+        elif (recommendation['price'] < 0 and recommendation['news'] >= 0):
+            conjunction = 'despite'
+        elif (recommendation['price'] >= 0 and recommendation['news'] < 0):
+            conjunction = 'but luckily'
+    else:
+        #recommendation
+        recommendation_yes = 'not '
+        recommendation_no = ''
+        # choose conjuction
+        if (recommendation['price'] < 0 and recommendation['news'] < 0):
+            conjunction = 'and'
+        elif (recommendation['price'] < 0 and recommendation['news'] >= 0):
+            conjunction = 'but'
+        elif (recommendation['price'] >= 0 and recommendation['news'] < 0):
+            conjunction = 'despite'
+
+
+    print_template("the news of {} is {} ({} %)".format(crypto, news_level, int(recommendation['news']*100)))
+    print_template("{}..".format(conjunction))
+    print_template("the price of {} is {} ({} %)".format(crypto, price_level, int(recommendation['price']*100)))
+
+    follow = input_template("Do you want to follow our recommendation? say 'buy' to {}follow otherwise to {}follow".format(recommendation_yes, recommendation_no))
+
+    if ("buy" in str(follow)):
+
+        #TODO ask how much
+        
+
+        follow_confirmation = input_template("Are you sure to buy? 'yes' to execute your order or otherwise to cancel".format(follow))
+        if ("yes" in str(follow_confirmation)):
+            #TODO buying
+            print('buying')
+            #TODO logging
+            print('logging')
+        else:
+            print_template(yellow_color('Your order has been cancelled'))
+            template_trade()
+    else:
+        print_template(yellow_color('Your order has been cancelled'))
+        template_trade()
+
 
 
 def template_account_balance():
     print("{}: Here is your balance details".format(system_name))
-    print(green_color("===="))
+    print(yellow_color("===="))
     account_response = gdax.get_account_balance()
     for currency_detail in account_response:
         if (float(currency_detail['balance']) > 0 ):
@@ -84,7 +192,7 @@ def template_account_balance():
             print("Balance: {}".format(currency_detail['balance']))
             print("Hold: {}".format(currency_detail['hold']))
             print("Available: {}".format(currency_detail['available']))
-            print(green_color("===="))
+            print(yellow_color("===="))
 
 
 def template_registration():
@@ -149,5 +257,6 @@ if __name__ == '__main__':
     main_function()
 
 
+#TODO complete buying
 #TODO history
 #TODO simulation
