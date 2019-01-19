6f5110223835991da8bbf3cd3eb983ab55befc38
Rens van Schouwenburg <rensvanschouwenburg@gmail.com>
2019-01-19 10:25:40 +0100

    Add indicators, newsreader, tweepy cursor

---
+++ indicators.py
@@ -0,0 +1,85 @@
+import api_key
+import requests
+import pandas as pd
+import talib
+import numpy as np
+
+
+url = "https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=EUR&limit=100&e=Coinbase" + "&api_key=" + api_key.API_KEY
+response = requests.get(url).json()
+df = pd.DataFrame(response["Data"])
+
+# Relative Strength Index
+def rsi(df):
+	rsi_lst = talib.RSI(df.close.values)
+	return round(rsi_lst[len(rsi_lst) - 1],2)
+
+# Average Directional Movement Index
+def adx(df):
+	adx_lst = talib.ADX(high=df.high.values, low=df.low.values, close=df.close.values, timeperiod=14)
+	return round(adx_lst[len(adx_lst) - 1],2)
+
+# Moving Average Converging Diverging
+def macd(df):
+	return talib.MACD(close=df.close.values, fastperiod=12, slowperiod=26, signalperiod=9)
+
+# converts the indicator to a number from a -1 to 1 interval
+def conv_rsi(indicator, buy=30, sell=70):
+	mean = abs((buy + sell) / 2)
+	result = ((indicator - mean) / (buy - mean))
+	if result > 1: result = 1
+	elif result < -1: result = -1
+	if buy > sell: result = -1 * result
+	return round(result, 2)
+
+# converts the indicator to a number from a -1 to 1 interval
+# indicators from 0 - 25 are non-existing trends, from 25-100 increasing strength
+def conv_adx(indicator):
+	if indicator < 25:
+		result = 0
+	else:
+		result = indicator / 100
+	return round(result,2)
+
+def interpretation(indicator, value):
+	if indicator == "adx":
+		if value > 0 and value < 25:
+			print("Trend is absent or weak")
+		elif value >= 25 and value < 50:
+			print("Trend is strong")
+		elif value >= 50 and value < 75:
+			print("Trend is very strong")
+		elif value >= 75 and value <= 100:
+			print("Trend is extremely strong")
+		else:
+			print("Value is out of range 0 - 100")
+# print(df)
+# print("RSI: " + str(rsi(df)) + "\nConverted: " + str(conv_rsi(rsi(df))))
+# print("ADX: " + str(adx(df)) + "\nConverted: " + str(conv_adx(adx(df))))	
+
+# Test Cases
+print("For 0 = buy and 100 = sell")
+r1 = round(np.random.random()*100, 0)
+print("RSI = " + str(r1) +", " + str(conv_rsi(r1, 0, 100)))
+r2 = round(np.random.random()*100, 0)
+print("RSI = " + str(r2) +", " + str(conv_rsi(r2, 0, 100)))
+r3 = round(np.random.random()*-100, 0)
+print("RSI = " + str(r3) +", " + str(conv_rsi(r3, 0, 100)))
+print("RSI = 30, " + str(conv_rsi(30, 0, 100)))
+print("RSI = 70, " + str(conv_rsi(70, 0, 100)))
+
+print("\nFor 30 = buy and 70 = sell")
+print("RSI = " + str(r1) +", " + str(conv_rsi(r1)))
+print("RSI = " + str(r2) +", " + str(conv_rsi(r2)))
+print("RSI = " + str(r3) +", " + str(conv_rsi(r3)))
+print("RSI = 30, " + str(conv_rsi(30)))
+print("RSI = 70, " + str(conv_rsi(70)))
+# print("RSI = 100, " + str(conv_rsi(100, 0, 100)))
+# print("RSI = 0, " + str(conv_rsi(0, 0, 100)))
+print("\n")
+print("ADX = " + str(r1) + ", " + str(conv_adx(r1)))
+interpretation("adx", r1)
+print("ADX = " + str(r2) +", " + str(conv_adx(r2)))
+interpretation("adx", r2)
+print("ADX = " + str(r3) +", " + str(conv_adx(r3)))
+interpretation("adx", r3)
---
+++ news_reader_cc.py
@@ -0,0 +1,34 @@
+import datetime
+import requests
+import pandas as pd
+import talib
+import numpy as np
+from textblob import TextBlob
+import api_key
+
+# Describes the sentiment of their tite and body of 34 different news sources 
+# i.e. CryptoGlobe, Coindesk, CoinTelegraph, CryptoInsider
+
+url = "https://min-api.cryptocompare.com/data/v2/news/?lang=EN" + "&api_key=" + api_key.API_KEY
+response = requests.get(url).json()
+df = pd.DataFrame(response["Data"])
+
+today = datetime.datetime.now().date() #.strftime("%Y-%m-%d")
+yesterday = today - datetime.timedelta(days=1)
+y_in_unix = (datetime.datetime.combine(yesterday, datetime.datetime.min.time()) - datetime.datetime(1970,1,1)).total_seconds()
+
+# Produce a list containing news objects that are
+	# published yesterday or today AND
+	# contains the query in the title or categories
+query = ["btc", "bitcoin"]
+result = []
+for obj in df.values:
+	for q in queries:
+		if obj[7] >= y_in_unix and (q in (obj[0].lower() or obj[1].lower())):
+			result.append(obj)
+sentiment = 0
+for article in result:
+	sentiment += TextBlob(article[0]).sentiment.polarity
+print("There are " + str(len(result)) + " articles found for the query: " + query)
+print("The total sentiment is: " + str(sentiment))
+print("The average sentiment is: " + str(round(sentiment / len(result), 2)))
